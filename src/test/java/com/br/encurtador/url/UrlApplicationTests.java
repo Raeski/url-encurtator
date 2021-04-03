9de801c33af5ed120c943f4c1dcea80d40d01c9a
Raeski <gustavoraeski@outlook.com>
2021-04-03 00:19:39 -0300

    change name TesteApplication for UrlApplication

--- src/test/java/com/br/encurtador/url/UrlApplicationTests.java
+++ src/test/java/com/br/encurtador/url/UrlApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TesteApplicationTests {
+class UrlApplicationTests {
 
     @Test
     void contextLoads() {
